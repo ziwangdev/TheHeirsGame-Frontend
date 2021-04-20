4fcc4005d8484ad2c0e2bbd0fe92194f7bc198b5
Zi Wang <wang821@usc.edu>
2021-04-19 17:42:56 -0700

    Update

---
+++ src/helpers/Helpers.js
@@ -0,0 +1,13 @@
+export function getUserPlayer(gameValue, userValue){
+    // Get user's player data
+    let userPlayer = null;
+    let players = gameValue.players;
+    let playersKeys = Object.keys(players);
+    for(var i = 0; i < playersKeys.length; i++){
+        let key = playersKeys[i];
+        if(players[key].name === userValue.name && players[key].character === userValue.character){
+            userPlayer = players[key];
+        }
+    }
+    return userPlayer;
+}

e27cea50766ad261f6934bb00cd96304f65ee659
Zi Wang <wang821@usc.edu>
2021-04-19 17:44:29 -0700

    Implement access to backend data

--- src/pages/components/Inventory.js
+++ src/pages/components/Inventory.js
@@ -2,11 +2,16 @@ import React, {useContext, useEffect, useState} from "react";
 import {GameContext} from "../../contexts/GameContext";
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import { getUserPlayer } from '../../helpers/Helpers';
 import jinwoo from '../../images/jinwoo.jpg';
 import hyojin from '../../images/hyojin.jpg';
 import yunho from '../../images/yunho.jpg';
 import hyori from '../../images/hyori.jpg';
+
 import cardDice from '../../images/card-dice.png';
+import cardNull from '../../images/card-null.png';
+import cardStop from '../../images/card-stop.png';
+
 import diceUnknown from '../../images/dice-unknown.png';
 import dice1 from '../../images/1.png';
 import dice2 from '../../images/2.png';
@@ -21,11 +26,34 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
 
+function cardNameToModule(cardName){
+    if(cardName === 'card-dice'){
+        return cardDice;
+    } else if(cardName === 'card-null'){
+        return cardNull;
+    }
+    else if(cardName === 'card-stop'){
+        return cardStop;
+    }
+
+}
+
 export default function Inventory(){
 
     const {game, user} = useContext(GameContext);
+    const [gameValue, setGameValue] = game;
     const [userValue, setUserValue] = user;
     const [dice, setDice] = useState('?');
+    const [cards, setCards] = useState({
+        card1: cardNull,
+        card2: cardNull,
+        card3: cardNull,
+        card4: cardNull,
+        card5: cardNull,
+        card6: cardNull,
+        card7: cardNull,
+        card8: cardNull
+    });
     const [avatar, setAvatar] = useState(jinwoo);
 
     const rollDice = () => {
@@ -52,7 +80,9 @@ export default function Inventory(){
     };
 
     useEffect(() => {
+        console.log('useEffect in inventory.js');
         if(userValue){
+            // Find avatar
             let character = userValue.character;
             if(character === '金振宇'){
                 setAvatar(jinwoo);
@@ -66,8 +96,22 @@ export default function Inventory(){
             else if(character === '金允浩  '){
                 setAvatar(yunho);
             }
+            // Get user's player data
+            let userPlayer = getUserPlayer(gameValue, userValue);
+            let userPlayerCards = userPlayer.cards;
+            console.log('database cards: ');
+            console.log(userPlayerCards);
+            let cardsKeys = Object.keys(userPlayerCards);
+            for(var i = 0; i < cardsKeys.length; i++){
+                let updatedCards = cards;
+                updatedCards[cardsKeys[i]] = cardNameToModule(userPlayerCards[cardsKeys[i]]);
+                console.log('updated cards:');
+                console.log(updatedCards);
+                setCards(updatedCards);
+            }
         }
-
+        console.log('after updating cards state: ')
+        console.log(cards);
     });
 
     return(
@@ -78,28 +122,28 @@ export default function Inventory(){
             </div>
             <Row className={'card-container'}>
                 <div className={'card'} id={'card-1'}>
-                    <img src={cardDice} alt={'Card 1'}/>
+                    <img src={cards.card1} alt={'Card 1'}/>
                 </div>
                 <div className={'card'} id={'card-2'}>
-                    <img src={cardDice} alt={'Card 2'}/>
+                    <img src={cards.card2} alt={'Card 2'}/>
                 </div>
                 <div className={'card'} id={'card-3'}>
-                    <img src={cardDice} alt={'Card 3'}/>
+                    <img src={cards.card3} alt={'Card 3'}/>
                 </div>
                 <div className={'card'} id={'card-4'}>
-                    <img src={cardDice} alt={'Card 4'}/>
+                    <img src={cards.card4} alt={'Card 4'}/>
                 </div>
                 <div className={'card'} id={'card-5'}>
-                    <img src={cardDice} alt={'Card 5'}/>
+                    <img src={cards.card5} alt={'Card 5'}/>
                 </div>
                 <div className={'card'} id={'card-6'}>
-                    <img src={cardDice} alt={'Card 6'}/>
+                    <img src={cards.card6} alt={'Card 6'}/>
                 </div>
                 <div className={'card'} id={'card-7'}>
-                    <img src={cardDice} alt={'Card 7'}/>
+                    <img src={cards.card7} alt={'Card 7'}/>
                 </div>
                 <div className={'card'} id={'card-8'}>
-                    <img src={cardDice} alt={'Card 8'}/>
+                    <img src={cards.card8} alt={'Card 8'}/>
                 </div>
             </Row>
             {/*<Wallet/>*/}

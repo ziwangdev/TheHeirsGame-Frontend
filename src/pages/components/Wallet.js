7fb8ab527aeefa5c9fcdc4808c300df87dfe31c9
Zi Wang <wang821@usc.edu>
2021-04-19 17:43:58 -0700

    Implement access to backend data

--- src/pages/components/Wallet.js
+++ src/pages/components/Wallet.js
@@ -1,16 +1,46 @@
-import React, {Component} from 'react'
+import React, {useState, useContext, useEffect} from 'react'
+import {GameContext} from "../../contexts/GameContext";
+import cashIcon from '../../images/wealth-cash.png'
+import propertyIcon from '../../images/wealth-property.png'
 
 import '../../styles/Walllet.css'
 
-class Wallet extends Component{
-  render(){
-      return(
-          <div className={'wallet-container'}>
-              <h6>现金 $200,000</h6>
-              <h6>总资产 $200,000</h6>
-          </div>
-      );
-  }
-}
+export default function Wallet(){
+    const {game, user} = useContext(GameContext)
+    const [gameValue, setGameValue] = game;
+    const [userValue, setUserValue] = user;
+    const [cash, setCash] = useState(0);
+    const [property, setProperty] = useState(0);
 
-export default Wallet
+    useEffect(()=>{
+        if(userValue){
+            // Get user's player data
+            let userPlayer = null;
+            let players = gameValue.players;
+            let playersKeys = Object.keys(players);
+            for(var i = 0; i < playersKeys.length; i++){
+                let key = playersKeys[i];
+                if(players[key].name === userValue.name && players[key].character === userValue.character){
+                    userPlayer = players[key];
+                }
+            }
+            // Set wallet
+            setCash(userPlayer.cash);
+            setProperty(userPlayer.property);
+        }
+    })
+
+  return(
+      <div className={'wallet-container'}>
+          <div className={'wallet-player-wealth'}>
+              <img src={cashIcon} alt={'Cash'}/>
+              <h6>${cash}</h6>
+          </div>
+          <div className={'wallet-player-wealth'}>
+              <img src={propertyIcon} alt={'Property'}/>
+              <h6>${property}</h6>
+          </div>
+      </div>
+  );
+
+}
